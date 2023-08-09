d0f884ee4af0b400f2a6d6df5e6fc6aaf13ed504
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-08-09 15:42:37 +0530

    Remove Task : Create 'X' Button to remove item

--- script.js
+++ script.js
@@ -5,22 +5,31 @@ console.log("To Do List");
 function addItem() {
     var inValue = document.getElementById('addTask');
     var task = inValue.value;
-    console.log(task);
 
     var listGroup = document.getElementById("listGroup")
 
     const li = document.createElement("li");
     li.textContent = task;
 
-
+    const a = document.createElement('a');
+    a.textContent = 'X'
+    li.appendChild(a)
 
     const insertFrist = listGroup.firstElementChild;
-    if(insertFrist == null){
+    if (insertFrist == null) {
         listGroup.appendChild(li)
-    }else{
+    } else {
         listGroup.insertBefore(li, insertFrist)
     }
 
     inValue.value = '';
     inValue.focus()
 }
+
+let delBtn = document.querySelector('ul');
+delBtn.addEventListener('click', (e) => {
+    let listGroup = document.getElementById('listGroup');
+    let li = e.target.parentNode;
+
+    listGroup.removeChild(li);
+})

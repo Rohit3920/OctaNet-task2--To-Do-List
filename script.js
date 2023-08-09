702e0fcb8719c35901082cf37f0805090ec1cae2
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-08-09 14:50:00 +0530

    AddTask Event : Click to button add task (li tag add in ul tag) in the list, and styled in task list

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 console.warn("Octa Net Task No. 2");
-console.log("To Do List")
+console.log("To Do List");
+
+
+function addItem() {
+    var inValue = document.getElementById('addTask');
+    var task = inValue.value;
+    console.log(task);
+
+    var listGroup = document.getElementById("listGroup")
+
+    const li = document.createElement("li");
+    li.textContent = task;
+
+    listGroup.appendChild(li)
+}
